08bda1424cc1b92a8eb8560df511121f9f1f65d9
vikashPirth <vikash.pirthiani@gmail.com>
2024-10-17 00:57:39 +0200

    saving data in excel

--- finvizScraper.py
+++ finvizScraper.py
@@ -4,7 +4,30 @@ import pandas as pd
 from openpyxl import load_workbook
 from io import StringIO
 
+workbook = load_workbook('FinvizData.xlsx')
+sheet = workbook["Sheet1"]
 
+
+def findStartPoint():
+    index = 1
+    #if this loop is true, the current cell has a value inside of it
+    while(sheet.cell(row = index, column = 1).value != None):
+        index += 1
+    return index
+
+def putDataIntoSheet(df):
+    rowIndex = findStartPoint() - 1
+   
+    df = df.iloc[:, 1:]
+    for ticker in range(len(df)):
+        columnIndex = 1
+        rowIndex += 1
+        #find the row corresponding to the ticker index
+        tickerDetails = df.iloc[ticker]
+        for tickerDetail in tickerDetails:
+            sheet.cell(row = rowIndex, column=columnIndex).value = tickerDetail
+            columnIndex += 1
+    
 
 def getTotalPages(soup):
 
@@ -14,6 +37,7 @@ def getTotalPages(soup):
         print("Page is empyt")
         return
     totalPages = (len(paginationTags) - 1)
+    
     if (totalPages == 0):
         return 1
     else:
@@ -28,6 +52,7 @@ def getWebpage():
     
     numPage = getTotalPages(soup=soup)
     tickerNumber = 1
+    
     for numPage in range(numPage):
         response = requests.get(url= url+f"&r={tickerNumber}", headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -35,5 +60,8 @@ def getWebpage():
         table_html = StringIO(str(table))
         pdData = pd.read_html(table_html)
         tickerNumber+=20
+        putDataIntoSheet(pdData[0])
 
 getWebpage()
+
+workbook.save("FinvizData.xlsx")
